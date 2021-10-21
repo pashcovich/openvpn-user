fe604f117a9cec41ca328ca6ea87bf33a7c76f26
Ilya Sosnovsky <ilya.sosnovsky@flant.com>
2021-10-21 10:22:22 +0300

    fix install deps script

--- install-deps_arm.sh
+++ install-deps_arm.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y gcc gcc-multilib sqlite3 libc6-dev-arm64-cross gcc-arm-linux-gnueabi gcc-aarch64-linux-gnu
+apt-get install -y gcc-multilib libc6 libc6-dev libc6-dev-i386 sqlite3 libc6-dev-arm64-cross gcc-arm-linux-gnueabi gcc-aarch64-linux-gnu
